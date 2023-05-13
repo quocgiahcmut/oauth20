0215056d674dfd32c94876abdf0a84456a9b3c67
Gia Duong <duonggia31@gmail.com>
2023-05-13 09:29:55 +0700

    init commit

---
+++ OAuth20.Server/Models/Client.cs
@@ -0,0 +1,21 @@
+﻿namespace OAuth20.Server.Models;
+
+public class Client
+{
+    public Client()
+    {
+
+    }
+
+    public string ClientName { get; set; }
+    public string ClientId { get; set; }
+
+    public string ClientSecret { get; set; }
+    public IList<string> GrantType { get; set; }
+
+    public bool IsActive { get; set; } = false;
+    public IList<string> AllowedScopes { get; set; }
+
+    public string ClientUri { get; set; }
+    public string RedirectUri { get; set; }
+}
---
+++ OAuth20.Server/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OAuth20.Server.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
